4b3c7bc15daf758b955f13126b7c6ff67c63a91e
salsabiladcy <salsabiladiba@apps.ipb.ac.id>
2024-07-16 21:51:55 +0700

    beranda

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Beranda</title>
+</head>
+<body>
+    <!-- Awal Header  -->
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <img src="path-to-your-logo.png" alt="Logo" width="50" height="50">
+
+            </a>
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Beranda</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Portofolio
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#pengalamanprojek">Pengalaman Projek</a></li>
+                        <li><a class="dropdown-item" href="#produk">Produk</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a href="#" class="nav-link">Profil</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Kontak</a></li>
+            </ul>
+        </header>
+    </div>    
+    <!-- Akhir Header -->
+</body>
+</html>
